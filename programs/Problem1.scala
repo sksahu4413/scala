20253520e22949c17fe8178861c85b9d716d2aa1
Sonu Kumar <59005060+sksahu4413@users.noreply.github.com>
2025-03-23 13:21:53 +0530

    Updated Name

--- programs/Problem1.scala
+++ programs/Problem1.scala
@@ -4,8 +4,8 @@ Problem 1 : Write a program to print Hello, World! on the console.
 
 */
 
-object Problem1{
+object Problem1{ 
     def main(args: Array[String]){
-        println("Hello, World!")
+        println("Sonu Kumar Sahu!")
     }
 }
